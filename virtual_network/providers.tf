218840d52778255e7a2ddead4a6d42866cbcf955
Hans Lindgren <hans.lindgren@kgk.se>
2022-06-07 10:54:47 +0200

    Bump some version constraints

--- virtual_network/providers.tf
+++ virtual_network/providers.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.75"
+      version = ">= 3.1.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
