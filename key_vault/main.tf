e3cee60e76f36ff36fb23462d23e24864f6380e5
Hans Lindgren <hans.lindgren@kgk.se>
2023-04-17 09:21:09 +0200

    Additional key vault settings

--- key_vault/main.tf
+++ key_vault/main.tf
@@ -20,8 +20,13 @@ locals {
     )
 
     sku_name                        = try(local.env_config.key_vault.sku_name, var.config.global.key_vault.sku_name, null) // standard or premium
+    enable_rbac_authorization       = try(local.env_config.key_vault.enable_for_rbac_authorization, var.config.global.key_vault.enable_for_rbac_authorization, null)
+    enabled_for_deployment          = try(local.env_config.key_vault.enabled_for_deployment, var.config.global.key_vault.enabled_for_deployment, null)
+    enabled_for_disk_encryption     = try(local.env_config.key_vault.enabled_for_disk_encryption, var.config.global.key_vault.enabled_for_disk_encryption, null)
     enabled_for_template_deployment = try(local.env_config.key_vault.enabled_for_template_deployment, var.config.global.key_vault.enabled_for_template_deployment, true)
     purge_protection_enabled        = try(local.env_config.key_vault.purge_protection_enabled, var.config.global.key_vault.purge_protection_enabled, false)
+    soft_delete_retention_days      = try(local.env_config.key_vault.soft_delete_retention_days, var.config.global.key_vault.soft_delete_retention_days, 90)
+    public_network_access_enabled   = try(local.env_config.key_vault.public_network_access_enabled, var.config.global.key_vault.public_network_access_enabled, false)
 
     network_acls = {
       bypass                     = try(local.env_config.key_vault.network_acls.bypass, var.config.global.key_vault.network_acls.bypass, "AzureServices")
@@ -59,9 +64,14 @@ resource "azurerm_key_vault" "this" {
   tags                            = local.config.tags
   tenant_id                       = data.azurerm_client_config.this.tenant_id
   sku_name                        = local.config.sku_name
+  enable_rbac_authorization       = local.config.enable_rbac_authorization
+  enabled_for_deployment          = local.config.enabled_for_deployment
+  enabled_for_disk_encryption     = local.config.enabled_for_disk_encryption
   enabled_for_template_deployment = local.config.enabled_for_template_deployment
   #tfsec:ignore:azure-keyvault-no-purge
   purge_protection_enabled        = local.config.purge_protection_enabled
+  soft_delete_retention_days      = local.config.soft_delete_retention_days
+  public_network_access_enabled   = local.config.public_network_access_enabled
 
   network_acls {
     bypass = local.config.network_acls.bypass
