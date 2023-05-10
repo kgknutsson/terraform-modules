657740c64518bb8a83594ec9a3708f3da7cb7964
Elias Ericsson-Rydberg <elias.ericsson.rydberg@gmail.com>
2023-05-10 14:04:37 +0200

    Configure User Assigned Identity to be used for Key Vault references

--- app_service/main.tf
+++ app_service/main.tf
@@ -476,6 +476,8 @@ resource "azurerm_linux_web_app" "this" {
       tags["hidden-link: /app-insights-resource-id"],
     ]
   }
+  
+  key_vault_reference_identity_id = azurerm_user_assigned_identity.this[0].id
 }
 
 resource "azurerm_linux_web_app_slot" "this" {
