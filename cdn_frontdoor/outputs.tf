de85d0f478db832561942ecdc78147a0316141d8
Hans Lindgren <hans.lindgren@kgk.se>
2023-11-29 16:48:26 +0100

    Changes to module output

--- cdn_frontdoor/outputs.tf
+++ cdn_frontdoor/outputs.tf
@@ -1,4 +1,9 @@
-output "cdn_frontdoor_profile_id" {
-  description = "The CDN Frontdoor Profile Id"
+output "profile_id" {
+  description = "CDN Front Door profile id"
   value       = try(azurerm_cdn_frontdoor_profile.this[0].id, null)
+}
+
+output "host_names" {
+  description = "CDN Front Door host names"
+  value       = concat(values(azurerm_cdn_frontdoor_endpoint.this)[*].host_name, values(azurerm_cdn_frontdoor_custom_domain.this)[*].host_name)
 }
