cedffe02752c641a9fdb50e2aefda818ffa958f0
Hans Lindgren <hans.lindgren@kgk.se>
2024-01-02 15:14:42 +0100

    Add diagonostic settings to apim and frontdoor modules

--- api_management/outputs.tf
+++ api_management/outputs.tf
@@ -2,3 +2,19 @@ output "public_ip_addresses" {
   value       = try(azurerm_api_management.this[0].public_ip_addresses, null)
   description = "Public IP addresses"
 }
+
+output "monitor_config" {
+  value = {
+    diagnostic_settings = try(
+      [
+        {
+          name               = "SendToLogAnalytics"
+          target_resource_id = azurerm_api_management.this[0].id
+          log_category_types = null
+          metrics            = null
+        }
+      ],
+      []
+    )
+  }
+}
--- cdn_frontdoor/outputs.tf
+++ cdn_frontdoor/outputs.tf
@@ -7,3 +7,19 @@ output "host_names" {
   description = "CDN Front Door host names"
   value       = concat(values(azurerm_cdn_frontdoor_endpoint.this)[*].host_name, values(azurerm_cdn_frontdoor_custom_domain.this)[*].host_name)
 }
+
+output "monitor_config" {
+  value = {
+    diagnostic_settings = try(
+      [
+        {
+          name               = "SendToLogAnalytics"
+          target_resource_id = azurerm_cdn_frontdoor_profile.this[0].id
+          log_category_types = null
+          metrics            = null
+        }
+      ],
+      []
+    )
+  }
+}
