d0ba8c31303017f7e8df5f3992cf978e7fc091f6
Hans Lindgren <hans.lindgren@kgk.se>
2024-01-03 16:28:09 +0100

    Add diagonostic settings to keyvault module

--- key_vault/outputs.tf
+++ key_vault/outputs.tf
@@ -2,3 +2,19 @@ output "id" {
   value       = try(azurerm_key_vault.this.0.id, null)
   description = "Key vault id."
 }
+
+output "monitor_config" {
+  value = {
+    diagnostic_settings = try(
+      [
+        {
+          name               = "SendToLogAnalytics"
+          target_resource_id = azurerm_key_vault.this.0.id
+          log_category_types = null
+          metrics            = null
+        }
+      ],
+      []
+    )
+  }
+}
