c54a167bb0c9bd755f92a0deef5ba26545ff70d3
Hans Lindgren <hans.lindgren@kgk.se>
2024-11-26 10:31:25 +0100

    Add dns_forwarding_ruleset_id to output

--- private_dns_resolver/outputs.tf
+++ private_dns_resolver/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   value       = try(azurerm_private_dns_resolver.this[0].id, null)
   description = "Private DNS resolver id."
 }
+
+output "dns_forwarding_ruleset_id" {
+  value       = try(azurerm_private_dns_resolver_dns_forwarding_ruleset.this[0].id, null)
+  description = "Private DNS resolver forwarding ruleset id"
+}
